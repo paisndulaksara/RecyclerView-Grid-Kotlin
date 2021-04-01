8a774079b1f37f945a5ec7130b6d62a320de3683
pasinduadelmo <pasindu@adelmotechnology.com>
2021-04-01 17:54:11 +0530

    recycler view

---
+++ app/src/main/java/com/adelmotechnology/recyclergrid/DataModel.kt
@@ -0,0 +1,4 @@
+package com.adelmotechnology.recyclergrid
+
+data class DataModel
+    (var title : String, var desc : String, var image : Int)
---
+++ app/src/main/java/com/adelmotechnology/recyclergrid/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.adelmotechnology.recyclergrid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var  photoAdapter: PhotoAdapter
+    private var dataList = mutableListOf<DataModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+//        val layoutManager = LinearLayoutManager(this)
+//        layoutManager.orientation = LinearLayoutManager.VERTICAL
+//        recyclerView.layoutManager = layoutManager
+//
+//        val adapter = HobbyAdapter(this, Supplier.hobbies)
+//        recyclerView.adapter = adapter
+
+
+        recyclerView.layoutManager = GridLayoutManager(applicationContext,2)
+        photoAdapter = PhotoAdapter(applicationContext)
+        recyclerView.adapter = photoAdapter
+
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+        dataList.add(DataModel("Title","Desc",R.drawable.online))
+
+        photoAdapter.setDataList(dataList)
+
+
+    }
+}
